c307777da8567bcbf42803f7b7cc582e4639a9c3
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-02-01 12:58:35 +0700

    Update luks.sh

--- luks.sh
+++ luks.sh
@@ -216,5 +216,6 @@ reformat() {
   [ "$fs_for_container" == "4" ] && mkfs.vfat -f /dev/mapper/secret_container
   [ "$fs_for_container" == "5" ] && mkfs.ntfs -f /dev/mapper/secret_container
 }
+set -e
 user_check
 menu
